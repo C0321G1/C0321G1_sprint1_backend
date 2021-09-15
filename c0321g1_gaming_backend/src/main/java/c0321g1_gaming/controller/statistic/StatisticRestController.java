958a735bc39797b6a0ef11635f317045138a4b14
Hau HP <hauhpdn2015@gmail.com>
2021-09-15 13:51:02 +0700

    them comment

--- c0321g1_gaming_backend/src/main/java/c0321g1_gaming/controller/statistic/StatisticRestController.java
+++ c0321g1_gaming_backend/src/main/java/c0321g1_gaming/controller/statistic/StatisticRestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
-
+// Create by HauHP
 @RestController
 @RequestMapping("statistic")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
--- c0321g1_gaming_backend/src/main/java/c0321g1_gaming/dto/statistic/StatisticByComputer.java
+++ c0321g1_gaming_backend/src/main/java/c0321g1_gaming/dto/statistic/StatisticByComputer.java
@@ -1,5 +1,5 @@
 package c0321g1_gaming.dto.statistic;
-
+// Create by HauHP
 public interface StatisticByComputer {
     Long getHour();
     String getComputer();
