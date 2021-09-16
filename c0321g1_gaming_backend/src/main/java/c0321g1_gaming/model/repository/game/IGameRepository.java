64209de2f958c3340125f8ee6b6ad24ad9932014
unknown <thuythuy100897@gmail.com>
2021-09-16 10:56:02 +0700

    merge code

--- c0321g1_gaming_backend/src/main/java/c0321g1_gaming/model/repository/game/IGameRepository.java
+++ c0321g1_gaming_backend/src/main/java/c0321g1_gaming/model/repository/game/IGameRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 import java.util.Optional;
 
