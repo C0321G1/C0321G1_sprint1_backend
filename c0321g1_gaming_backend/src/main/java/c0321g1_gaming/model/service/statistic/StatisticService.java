ae485be328a8bad61b91cf309681d501c326915d
Hau HP <hauhpdn2015@gmail.com>
2021-09-19 18:58:01 +0700

    fix smell code

--- c0321g1_gaming_backend/src/main/java/c0321g1_gaming/model/service/statistic/StatisticService.java
+++ c0321g1_gaming_backend/src/main/java/c0321g1_gaming/model/service/statistic/StatisticService.java
@@ -1,10 +1,8 @@
 package c0321g1_gaming.model.service.statistic;
 
-import c0321g1_gaming.dto.order.OrderDetailDto;
 import c0321g1_gaming.dto.statistic.StatisticByAccount;
 import c0321g1_gaming.dto.statistic.StatisticByComputer;
 import c0321g1_gaming.dto.statistic.StatisticByMonth;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 // Create by HauHP
